538670dceac9e046be9050ac9d674fe22e18ee91
Jacob Gervais <36945260+jacobg826@users.noreply.github.com>
2021-06-10 11:02:08 -0700

    Make website mobile compatible

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import myFace from './images/my_face.jpg'
+import './images.css'
 
 
 function Home () {
@@ -7,7 +8,7 @@ function Home () {
         <div>
             <div className="welcome">
                 <div className="text-box">
-                    <h1 className="text-style">
+                    <h1 className="text-styl">
                         Hello, my name is Jacob. I am an electrical engineer and software developer from Bellingham, Washington.
                     </h1>
                 </div>
